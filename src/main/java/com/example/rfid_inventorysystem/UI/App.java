5f777db4fdb19740a762cb48756bc4f5a7990cd7
George Boariu <georgeboariugb@gmail.com>
2023-10-12 19:29:39 +0300

    Resolved error from controllers. Empty functions

---
+++ src/main/java/com/example/rfid_inventorysystem/UI/App.java
@@ -0,0 +1,16 @@
+package com.example.rfid_inventorysystem.UI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
